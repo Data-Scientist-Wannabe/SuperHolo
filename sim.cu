3c78026398c10d98addbf51bd35b4e61831fc260
Larry King <lartking@gmail.com>
2018-03-07 00:39:42 +0000

    no point trial

--- sim.cu
+++ sim.cu
@@ -98,7 +98,7 @@ int main(void)
     cudaMemcpy(d_points, h_points, POINT_COUNT * sizeof(double4), cudaMemcpyHostToDevice);
 
     // call method with dumb syntax
-    simulation<<<dimGrid, dimBlock>>>(d_pattern, d_points, POINT_COUNT);
+    simulation<<<dimGrid, dimBlock>>>(d_pattern, d_points, 0);
 
     cudaDeviceSynchronize();
 
