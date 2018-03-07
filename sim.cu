cbc6c539e5ed76c45ab474f434fe2c6654623565
Larry King <lartking@gmail.com>
2018-03-07 00:13:39 +0000

    trial run

--- sim.cu
+++ sim.cu
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
+#include <math.h>
+#include "pattern.h"
 
 #define CUDART_2PI      6.2831853071795865e+0
 #define CUDART_PI       3.1415926535897931e+0
@@ -22,10 +24,10 @@
 #define N_THREADS   512     // Threads per block
 
 #define LAMBDA          632.8e-9
+#define LAMBDA_INV      1.580278128950695e6
 #define REF_BEAM_ANGLE  CUDART_PIO6
 
-
-#define PLANE_CONST (2.0 * / LAMBDA * sin(REF_BEAM_ANGLE))
+#define PLANE_CONST (2.0 * CUDART_PI / LAMBDA * sin(REF_BEAM_ANGLE))
 #define VAL_CONST   (CUDART_2PI / LAMBDA)
 
 __device__ double plane(double x)
@@ -42,7 +44,8 @@ __device__ double distance(double2 uv, double3 point)
 
 __device__ double val(double2 uv, double3 point)
 {
-    return sin((distance(uv, point) % LAMBDA) * VAL_CONST);
+    double d = distance(uv, point);
+    return sin(( d - LAMBDA * floor(d * LAMBDA_INV)) * VAL_CONST);
 }
 
 __device__ double intensity(double2 uv, double3 point)
@@ -53,18 +56,19 @@ __device__ double intensity(double2 uv, double3 point)
 
 __global__ void simulation(double * pattern, double4 * points, int count)
 {
-    for(int x = threadId.x; x < ARRAY_WIDTH; x += blockDim.x)
+    for(int x = threadIdx.x; x < ARRAY_WIDTH; x += blockDim.x)
     {
-        for(int y = blockId.x; y < ARRAY_HEIGHT; y+= gridDim.x)
+        for(int y = blockIdx.x; y < ARRAY_HEIGHT; y+= gridDim.x)
         {
-            double2 uv(x * ARRAY_WIDTH_INV * PATTERN_WIDTH, y * ARRAY_HEIGHT_INV * PATTERN_HEIGHT);
+            double2 uv = {x * ARRAY_WIDTH_INV * PATTERN_WIDTH, y * ARRAY_HEIGHT_INV * PATTERN_HEIGHT};
             int index = y * ARRAY_WIDTH + x;
 
             pattern[index]= plane(uv.x);
 
             for(int i = 0; i < count; i++)
             {
-                pattern[index] += points[i].w * val(uv, points[i]) * intensity(uv, points[i]);
+                double3 point = make_double3(points[i].x, points[i].y, points[i].z);
+                pattern[index] += points[i].w * val(uv, point) * intensity(uv, point);
             }
         }
     }
@@ -89,7 +93,7 @@ int main(void)
     cudaMalloc((void**)&d_pattern,  ARRAY_SIZE * sizeof(double));
     cudaMalloc((void**)&d_points,   POINT_COUNT* sizeof(double4));
 
-    h_points[0] = {PATTERN_WIDTH / 2.0, PATTERN_HEIGHT / 2.0, 0.002, 1.0};
+    h_points[0] = make_double4(PATTERN_WIDTH / 2.0, PATTERN_HEIGHT / 2.0, 0.002, 1.0);
 
     cudaMemcpy(d_points, h_points, POINT_COUNT * sizeof(double4), cudaMemcpyHostToDevice);
 
@@ -104,8 +108,8 @@ int main(void)
     cudaFree(d_pattern);
     cudaFree(d_points);
 
-    pat.export_bmp("sim.bmp")
-    pat.save("sim.out")
+    pat.export_bmp("sim.bmp");
+    pat.save("sim.out");
 
     return 0;
 }
